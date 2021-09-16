e1482977839384ae7638b62db22982b4167d7776
Victorien Fotsing <victorien.fotsing@itc-service.eu>
2021-09-16 09:32:34 +0200

    set connection context

--- src/server/index.ts
+++ src/server/index.ts
@@ -17,22 +17,39 @@ interface OCPPEvent {
   removeListener(event: ClientCommand, listener: (...args: any[]) => void): this;
 }
 
+export class RequestContextError extends Error {
+  constructor(message?: any) {
+    super(message);
+    this.name = "RequestContextError";
+  }
+}
+
+export type WSParams = {
+  id: string;
+}
+
 export default class OCPPServer extends EventEmitter implements OCPPEvent {
   public ws: Server;
 
   constructor(options?: ServerOptions, callback?: () => void) {
     super();
     this.ws = new Server(options, callback);
-    this.ws.on('connection', (ws) => {
+    this.ws.on('connection', (ws, req) => {
+      if (!req.url) throw new RequestContextError();
+      const id = req.url.substring(1);
+      const params: WSParams = {
+        id
+      }
       ws.on('message', (message) => {
         const msg = new Call();
         msg.parseString(message.toString());
         if (msg.action && Object.values(ClientCommand).includes(msg.action)) {
-          this.emit(msg.action, ws, msg);
+          this.emit(msg.action, ws, params, msg);
         }
       });
     });
   }
+
   send(data: CallResult) {
     this.ws.on('open', (ws: WebSocket) => {
       ws.send(data.toString())
