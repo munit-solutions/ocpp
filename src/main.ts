b63d789140f1de3c3fc0b24cd485cbbc0b2991eb
Victorien Fotsing <victorien.fotsing@itc-service.eu>
2021-09-16 09:32:34 +0200

    set connection context

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,121 @@
-//
+import OCPPServer from './server';
+import Authorize from './builder/req/Authorize';
+import AuthorizeResult from './builder/conf/Authorize';
+import BootNotification from './builder/req/BootNotification';
+import BootNotificationResult from './builder/conf/BootNotification';
+import CancelReservation from './builder/req/CancelReservation';
+import CancelReservationResult from './builder/conf/CancelReservation';
+import ChangeAvailability from './builder/req/ChangeAvailability';
+import ChangeAvailabilityResult from './builder/conf/ChangeAvailability';
+import ChangeConfiguration from './builder/req/ChangeConfiguration';
+import ChangeConfigurationResult from './builder/conf/ChangeConfiguration';
+import ClearCache from './builder/req/ClearCache';
+import ClearCacheResult from './builder/conf/ClearCache';
+import ClearChargingProfile from './builder/req/ClearChargingProfile';
+import ClearChargingProfileResult from './builder/conf/ClearChargingProfile';
+import DataTransfer from './builder/req/DataTransfer';
+import DataTransferResult from './builder/conf/DataTransfer';
+import DiagnosticsStatusNotification from './builder/req/DiagnosticsStatusNotification';
+import DiagnosticsStatusNotificationResult from './builder/conf/DiagnosticsStatusNotification';
+import FirmwareStatusNotification from './builder/req/FirmwareStatusNotification';
+import FirmwareStatusNotificationResult from './builder/conf/FirmwareStatusNotification';
+import GetCompositeSchedule from './builder/req/GetCompositeSchedule';
+import GetCompositeScheduleResult from './builder/conf/GetCompositeSchedule';
+import GetDiagnostics from './builder/req/GetDiagnostics';
+import GetDiagnosticsResult from './builder/conf/GetDiagnostics';
+import ClientCommand from './enum/ClientCommand';
+import AvailabilityStatus from './enum/AvailabilityStatus';
+import AvailabilityType from './enum/AvailabilityType';
+import CancelReservationStatus from './enum/CancelReservationStatus';
+import ChargingProfilePurposeType from './enum/ChargingProfilePurposeType';
+import ChargingRateUnitType from './enum/ChargingRateUnitType';
+import ClearCacheStatus from './enum/ClearCacheStatus';
+import ClearChargingProfileStatus from './enum/ClearChargingProfileStatus';
+import ConfigurationStatus from './enum/ConfigurationStatus';
+import DataTransferStatus from './enum/DataTransferStatus';
+import DiagnosticsStatus from './enum/DiagnosticsStatus';
+import ErrorCode from './enum/ErrorCode';
+import FirmwareStatus from './enum/FirmwareStatus';
+import GetCompositeScheduleStatus from './enum/GetCompositeScheduleStatus';
+import RegistrationStatus from './enum/RegistrationStatus';
+import Heartbeat from './builder/req/Heartbeat';
+import HeartbeatResult from './builder/conf/Heartbeat';
+import MeterValues from './builder/req/MeterValues';
+import MeterValuesResult from './builder/conf/MeterValues';
+import RemoteStartTransaction from './builder/req/RemoteStartTransaction';
+import RemoteStartTransactionResult from './builder/conf/RemoteStartTransaction';
+import RemoteStopTransaction from './builder/req/RemoteStopTransaction';
+import RemoteStopTransactionResult from './builder/conf/RemoteStopTransaction';
+import ReserveNow from './builder/req/ReserveNow';
+import ReserveNowResult from './builder/conf/ReserveNow';
+import Reset from './builder/req/Reset';
+import ResetResult from './builder/conf/Reset';
+import SendLocalList from './builder/req/SendLocalList';
+import SendLocalListResult from './builder/conf/SendLocalList';
+import SetChargingProfile from './builder/req/SetChargingProfile';
+import SetChargingProfileResult from './builder/conf/SetChargingProfile';
+import StartTransaction from './builder/req/StartTransaction';
+import StartTransactionResult from './builder/conf/StartTransactions';
+
+export {
+  OCPPServer,
+  // command list
+  Authorize,
+  AuthorizeResult,
+  BootNotification,
+  BootNotificationResult,
+  CancelReservation,
+  CancelReservationResult,
+  ChangeAvailability,
+  ChangeAvailabilityResult,
+  ChangeConfiguration,
+  ChangeConfigurationResult,
+  ClearCache,
+  ClearCacheResult,
+  ClearChargingProfile,
+  ClearChargingProfileResult,
+  DataTransfer,
+  DataTransferResult,
+  DiagnosticsStatusNotification,
+  DiagnosticsStatusNotificationResult,
+  FirmwareStatusNotification,
+  FirmwareStatusNotificationResult,
+  GetCompositeSchedule,
+  GetCompositeScheduleResult,
+  GetDiagnostics,
+  GetDiagnosticsResult,
+  Heartbeat,
+  HeartbeatResult,
+  MeterValues,
+  MeterValuesResult,
+  RemoteStartTransaction,
+  RemoteStartTransactionResult,
+  RemoteStopTransaction,
+  RemoteStopTransactionResult,
+  ReserveNow,
+  ReserveNowResult,
+  Reset,
+  ResetResult,
+  SendLocalList,
+  SendLocalListResult,
+  SetChargingProfile,
+  SetChargingProfileResult,
+  StartTransaction,
+  StartTransactionResult,
+  // enum
+  ClientCommand,
+  AvailabilityStatus,
+  AvailabilityType,
+  CancelReservationStatus,
+  ChargingProfilePurposeType,
+  ChargingRateUnitType,
+  ClearCacheStatus,
+  ClearChargingProfileStatus,
+  ConfigurationStatus,
+  DataTransferStatus,
+  DiagnosticsStatus,
+  ErrorCode,
+  FirmwareStatus,
+  GetCompositeScheduleStatus,
+  RegistrationStatus
+};
